db72cd12765895e8d436fc87dae82aaea8988558
Akash Geethanjana <akashgeethanjana@gmail.com>
2022-04-03 10:23:35 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const navbar = document.querySelector('.navbar');
+
+document.addEventListener('scroll', () => {
+  if (window.scrollY > 5) {
+    let classesToAdd = ['bg-slate-900', 'backdrop-blur-xl', 'py-5'];
+    navbar.classList.remove('lg:py-10');
+    navbar.classList.add(...classesToAdd);
+  } else {
+    let classesToRemove = ['bg-slate-900', 'backdrop-blur-xl', 'py-5'];
+    navbar.classList.remove(...classesToRemove);
+    navbar.classList.add('lg:py-10');
+  }
+});
